fcb12bdc7670340d4af07a826a5a6cf0b69c9891
manuel <manuelen12@gmail.com>
2018-01-03 12:15:00 -0400

    get users

---
+++ login/GetUsers.php
@@ -0,0 +1,19 @@
+<?php
+		include '../conexion.php';
+		/* check connection */
+		if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+		}		
+
+		$mysqli->query("SET NAMES 'utf8'");
+		$sql="SELECT * FROM login";
+		$result=$mysqli->query($sql);
+		while($e=mysqli_fetch_assoc($result)){
+		$output[]=$e; 
+		}	
+
+		print(json_encode($output)); 
+		$mysqli->close();	
+
+		?>		
