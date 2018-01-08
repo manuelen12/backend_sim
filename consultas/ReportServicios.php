89a3776435146c16c704a400ec73872df599ff31
manuel <manuel@localhost.localdomain>
2018-01-08 16:26:06 -0400

    falta lo de mes y año, perfil, classif_incidentes

---
+++ consultas/ReportServicios.php
@@ -0,0 +1,39 @@
+<?php
+		include '../conexion.php';
+		/* check connection */
+		if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+		}
+		
+		if (isset($_GET['IdServicio'])){
+		$IdServicio = $_GET['IdServicio'];
+		if (!$IdServicio){
+			http_response_code(404);
+			echo "IdServicio es Requerido";
+			return;
+
+		}
+	}else{
+		http_response_code(404);
+		echo "IdServicio es Requerido";
+		return;
+	}
+	
+	$json=array();
+
+	if(isset($_GET["IdServicio"])){
+		
+		$IdServicio=$_GET['IdServicio'];
+
+		$mysqli->query("SET NAMES 'utf8'");
+		$sql="SELECT IdServicio*100/(SELECT sum(IdServicio) FROM detallereport) FROM detallereport";
+		$result=$mysqli->query($sql);
+		while($e=mysqli_fetch_assoc($result)){
+		$output[]=$e; 
+		}	
+
+	}
+		print(json_encode($output)); 
+		$mysqli->close();	
+		?>		
